43d6674600199b0a7eaacb2f613cb3b45e0ecadc
ATuanTran <tuantran112004@gmail.com>
2024-01-05 15:06:18 +0700

    create main.c
    bản mới tạo

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>  
+#include<conio.h> 
+int main()
+{
+	printf("*********************************************\n"); 
+	printf("* MSSV: PS31121                             *\n");
+	printf("* HoTen: Tran Anh Tuan                      *\n");      
+	printf("* Toi thich: nghe nhac, xem phim, choi game *\n");   
+	printf("* Toi khong thich: an rau, ca phe           *\n");   
+	printf("*********************************************"); 
+	getch();
+	return 0;
+} 
